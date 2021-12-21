ff734a82025a9e5b9a6ee4837459ba227e2b9674
André Schäfer <andre.schaefer90@gmail.com>
2021-12-21 20:03:44 +0100

    Add basic circle for future progress display

--- src/components/CountdownTimer.vue
+++ src/components/CountdownTimer.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="CountdownTimer">
+  <div class="countdown-timer">
     <h1>{{ msg }}</h1>
-    <h1>{{ formatTime(remainingSeconds) }}</h1>
+    <svg viewBox="0 0 100 100">
+      <g class="base-timer-circle">
+        <circle class="base-timer-path-elapsed" cx="50" cy="50" r="45" />
+      </g>
+    </svg>
+    <span>
+      <h1>{{ formatTime(remainingSeconds) }}</h1>
+    </span>
   </div>
   <div v-if="!intervalID">
     <input v-model="timeoutInMinutes" placeholder="Timeout in minutes" />
@@ -29,16 +36,18 @@ export default {
   },
   methods: {
     startTimer(durationInMinutes) {
-      this.remainingSeconds = durationInMinutes * 60;
+      if (durationInMinutes > 0) {
+        this.remainingSeconds = durationInMinutes * 60;
 
-      const that = this;
-      this.intervalID = setInterval(function () {
-        if (that.remainingSeconds === 0) {
-          that.stopTimer();
-          return;
-        }
-        that.remainingSeconds -= 1;
-      }, 1000);
+        const that = this;
+        this.intervalID = setInterval(function () {
+          if (that.remainingSeconds === 0) {
+            that.stopTimer();
+            return;
+          }
+          that.remainingSeconds -= 1;
+        }, 1000);
+      }
     },
     stopTimer() {
       clearInterval(this.intervalID);
@@ -64,18 +73,38 @@ export default {
 h1 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
+
 .controlButton {
   margin: 40px 0 0;
+}
+
+.countdown-timer {
+  position: inline;
+  height: 300px;
+  width: 300px;
+}
+
+.base-timer-circle {
+  fill: none;
+  stroke: none;
+}
+
+.base-timer-path-elapsed {
+  stroke-width: 7px;
+  stroke: grey;
 }
 </style>
