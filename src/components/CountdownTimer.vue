bbe4552763a4859fe8125bea687cf6d7918b8577
André Schäfer <andre.schaefer90@gmail.com>
2021-12-22 07:50:29 +0100

    Temporary css changes to keep elements aligned and centered

--- src/components/CountdownTimer.vue
+++ src/components/CountdownTimer.vue
@@ -6,19 +6,21 @@
         <circle class="base-timer-path-elapsed" cx="50" cy="50" r="45" />
       </g>
     </svg>
-    <span>
-      <h1>{{ formatTime(remainingSeconds) }}</h1>
+    <span class="base-timer-label">
+      {{ formatTime(remainingSeconds) }}
     </span>
   </div>
-  <div v-if="!intervalID">
-    <input v-model="timeoutInMinutes" placeholder="Timeout in minutes" />
-    <button class="controlButton" v-on:click="startTimer(timeoutInMinutes)">
-      Start Timer
+  <div class="controls">
+    <div v-if="!intervalID">
+      <input v-model="timeoutInMinutes" placeholder="Timeout in minutes" />
+      <button class="controlButton" v-on:click="startTimer(timeoutInMinutes)">
+        Start Timer
+      </button>
+    </div>
+    <button class="controlButton" v-else v-on:click="stopTimer()">
+      Stop Timer
     </button>
   </div>
-  <button class="controlButton" v-else v-on:click="stopTimer()">
-    Stop Timer
-  </button>
 </template>
 
 <script>
@@ -71,7 +73,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1 {
-  margin: 40px 0 0;
+  margin: 40px 0 40px 0;
 }
 
 ul {
@@ -88,12 +90,17 @@ a {
   color: #42b983;
 }
 
+.controls {
+  margin: 100px 0 0;
+}
+
 .controlButton {
   margin: 40px 0 0;
 }
 
 .countdown-timer {
   position: inline;
+  margin: auto;
   height: 300px;
   width: 300px;
 }
@@ -106,5 +113,24 @@ a {
 .base-timer-path-elapsed {
   stroke-width: 7px;
   stroke: grey;
+}
+
+.base-timer-label {
+  position: absolute;
+
+  /* Size should match the parent container */
+  width: 300px;
+  height: 300px;
+
+  /* Keep the label aligned to the top */
+  top: 140px;
+
+  /* Create a flexible box that centers content vertically and horizontally */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  /* Sort of an arbitrary number; adjust to your liking */
+  font-size: 48px;
 }
 </style>
